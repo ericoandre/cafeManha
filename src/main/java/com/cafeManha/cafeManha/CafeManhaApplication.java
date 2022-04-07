9f914c309f2f8ccfeb70aa1af5cdeeb48ea75eca
ericoandre <ericoandresilva@gmail.com>
2022-04-07 13:50:39 -0300

    inicial 🎉️

---
+++ src/main/java/com/cafeManha/cafeManha/CafeManhaApplication.java
@@ -0,0 +1,12 @@
+package com.cafeManha.cafeManha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManhaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManhaApplication.class, args);
+		System.out.println("🚀");
+	}
+}
---
+++ src/test/java/com/cafeManha/cafeManha/CafeManhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeManha.cafeManha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
