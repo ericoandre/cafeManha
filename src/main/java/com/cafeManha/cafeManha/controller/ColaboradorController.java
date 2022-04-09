1b9f4797fe5836ce808a01207dc79f914e260120
unknown <ericoandresilva@gmail.com>
2022-04-09 18:59:39 -0300

    cors

--- src/main/java/com/cafeManha/cafeManha/controller/ColaboradorController.java
+++ src/main/java/com/cafeManha/cafeManha/controller/ColaboradorController.java
@@ -8,6 +8,7 @@ import com.cafeManha.cafeManha.repository.ColaboradorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +22,7 @@ public class ColaboradorController {
     @Autowired
 	ColaboradorRepository colaboradorRepository;
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "*")
     @GetMapping("/colaborador")
 	public ResponseEntity<List<Colaborador>> getAllColaborador() {
         try{
